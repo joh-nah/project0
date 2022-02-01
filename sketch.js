acc0af31b9503a2962dec8667b3199fd9e38ddfa
joh-nah <johnahgarcia@gmail.com>
2022-02-01 12:03:56 -0800

    updated sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(800, 600);
+  
 }
+
 
 function draw() {
   background(0);
+  triangle(0, 0, 500, 600, 800, 0);  //x1,y1, x2, y2, x3, y3
+
+  push();
+  fill(255, 0, 0);
+  noStroke();
+  ellipse(58, 50, 25);  //x, y, width, optional-height
+  //circle(58, 50, 25);  //x, y, diameter
+  pop();
 
 }
